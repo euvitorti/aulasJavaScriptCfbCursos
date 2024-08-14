09ddf5551c064d1afb6a51f51461c3bde3a9694f
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-14 11:14:34 -0300

    Aula 5

---
+++ aulas/Operadores Relacionais - Aula 5/script.js
@@ -0,0 +1,31 @@
+// São operadores que vão retorna se o valor é verdadeiro ou não
+
+/*
+    >
+    <
+    >=
+    <=
+    ==
+    !=
+    || - OR
+    && - AND
+    ! - not não
+*/
+
+var a = 6
+var b = 5
+
+// SE (A) NÃO FOR MAIOR QUE B
+if (!(a > b)) {
+    console.log('É MENOR')
+} else {
+    console.log("É MAIOR")
+}
+
+let verdade = true;
+
+if (verdade) {
+    console.log('É verdade')
+} else {
+    console.log('Não é verdade')
+}
