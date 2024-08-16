b0dd6f52515ae23b47fecff248f67975c31220e7
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-16 11:20:56 -0300

    Aula 15-18

---
+++ aulas/Aula 15...18 - Estrutura de Repetição/script.js
@@ -0,0 +1,26 @@
+// ITERAÇÃO
+
+for (c = 0; c <= 10; c++) {
+    if (c == 0) {
+        console.log("Zero é neutro");
+        break
+    }
+    if (c % 2 == 0) {
+        console.log(`${c} é par.`);
+    } else {
+        console.log(`${c} é ímpar`);
+    }
+}
+
+// FOR IN E FOR OF
+
+let num = [10, 20, 30, 40, 50];
+
+for (n in num) {
+    console.log(n);
+}
+
+// ELE FOI NOS VALORES DO ARRAY,  NO IN TEM QUE USAR O NUM[I] - IDENTIFICAR A POSIÇÃO
+for (n of num) {
+    console.log(n);
+}
