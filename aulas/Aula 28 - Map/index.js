ebf138cb7a16dcecc8ab32f179bce9883ab710e3
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-21 10:53:22 -0300

    Aula 28

---
+++ aulas/Aula 28 - Map/index.js
@@ -0,0 +1,21 @@
+const caixa = document.querySelector("#caixa");
+const div = document.getElementsByTagName("div");
+console.log(div);
+
+// let mapa = new Map();
+
+// mapa.set("curso", "JavaScript");
+// mapa.set("canal", "CFBCursos");
+// mapa.set("nota", 10);
+
+// console.log(mapa);
+
+// mapa.delete("canal");
+
+// if (mapa.has("canal")) {
+//     caixa.innerHTML = `A CHAVE EXISTE NA COLEÇÃO COM O VALOR ${mapa.get("canal")}.`;
+// } else {
+//     caixa.innerHTML = "A CHAVE NÃO EXISTE NA COLEÇÃO";
+// }
+
+// caixa.innerHTML = mapa.get("curso");
