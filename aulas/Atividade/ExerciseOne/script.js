1f9f43d28a1ffb2bba4501bc8ce276352532e0db
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-23 10:56:21 -0300

    Primeira atividade

--- aulas/Atividade/ExerciseOne/script.js
+++ aulas/Atividade/ExerciseOne/script.js
@@ -1,24 +1,45 @@
-const primeiraCaixa = document.querySelector("caixa1");
-const segundaCaixa = document.querySelector("caixa2");
+const primeiraCaixa = document.querySelector("#caixa1");
+const segundaCaixa = document.querySelector("#caixa2");
 const botao = document.querySelector("#btn_copiar");
-const todosCursos = [...document.querySelectorAll(".cursoc1")];
+const todosCursos = [...document.querySelectorAll(".curso")];
 
-todosCursos.map((elementos) => {
-    elementos.addEventListener("click", (evento) => {
+// Itera sobre cada elemento do array `todosCursos` utilizando o método `forEach`
+todosCursos.forEach((elemento) => {
+
+    // Adiciona um event listener para o evento de clique em cada elemento do array `todosCursos`
+    elemento.addEventListener("click", (evento) => {
+
+        // Armazena o elemento que foi clicado na variável `curso`
         const curso = evento.target;
+
+        // Alterna a classe "destaque" no elemento clicado;
+        // Adiciona a classe se ela não estiver presente, ou a remove se ela estiver presente
         curso.classList.toggle("destaque");
     });
 });
 
+// Adiciona um event listener para o evento de clique no botão `botao`
 botao.addEventListener("click", () => {
-    const cursoSelecionados = [...document.querySelectorAll(".destaque")];
-    const cursoNaoSelecionados = [...document.querySelectorAll(".curso:not(.destaque)")];
 
-    cursoSelecionados.map((el) => {
-        caixa2.appendChild(el);
+    // Seleciona todos os elementos com a classe "destaque" e os converte para um array
+    // Armazenando-o na variável `cursosSelecionados`
+    const cursosSelecionados = [...document.querySelectorAll(".destaque")];
+
+    // Seleciona todos os elementos com a classe "curso" que não possuem a classe "destaque"
+    // Converte o NodeList resultante em um array e o armazena na variável `cursosNaoSelecionados`    
+    const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.destaque)")];
+
+    // Itera sobre cada elemento do array `cursosSelecionados` utilizando o método `forEach`
+    cursosSelecionados.forEach((el) => {
+
+        // Move cada elemento `el` selecionado para o contêiner `segundaCaixa`
+        segundaCaixa.appendChild(el);
     });
 
-    cursoNaoSelecionados.map((el) => {
-        caixa1.appendChild(el);
+    // Itera sobre cada elemento do array `cursosNaoSelecionados` utilizando o método `forEach`
+    cursosNaoSelecionados.forEach((el) => {
+
+        // Move cada elemento `el` não selecionado de volta para o contêiner `primeiraCaixa`
+        primeiraCaixa.appendChild(el);
     });
 });
