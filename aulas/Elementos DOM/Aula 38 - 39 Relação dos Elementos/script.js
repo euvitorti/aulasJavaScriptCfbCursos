07c75c05396d2f55899d79a32ab00decb310b40c
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-27 10:59:22 -0300

    Elementos DOM

---
+++ aulas/Elementos DOM/Aula 38 - 39 Relação dos Elementos/script.js
@@ -0,0 +1,16 @@
+const caixa = document.querySelector("#caixa1");
+const c1 = document.querySelector("#c1_child");
+const cursos = [...document.querySelectorAll(".curso")];
+
+console.log(c1.parentElement.parentElement);
+
+// console.log(cursos);
+// console.log(caixa.children);
+// console.log(caixa.firstElementChild);
+// console.log(caixa.lastElementChild);
+
+// console.log(document.getRootNode());
+// console.log(caixa.hasChildNodes());
+
+// caixa.firstElementChild.innerHTML = "Java";
+// caixa.children[1].innerHTML = "C#";
